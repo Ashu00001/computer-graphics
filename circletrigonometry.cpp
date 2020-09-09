695052a1f4fdf96b2fa49563145fe429723ca8c7
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-09-09 12:09:32 +0530

    Create circletrigonometry.cpp

---
+++ circletrigonometry.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<conio.h>
+#include<graphics.h>
+#include<math.h>
+#include<dos.h>
+#define round(a) ((int)(a+0.5))
+using namespace std;
+void circlepoly(float xc,float yc,float r)
+{
+float T =0,Tend =45,Tinc = 1/r,angle=0,x,y,t=3.14/180;
+    x=xc+r*cos(angle);
+	y=yc+r*sin(angle);
+while(angle<=45)
+{
+	x=r*cos(angle);
+	y=r*sin(angle);
+putpixel(xc+x,yc+y,WHITE);
+	putpixel(xc-x,yc+y,WHITE);
+	putpixel(xc+x,yc-y,WHITE);
+	putpixel(xc-x,yc-y,WHITE);
+	putpixel(xc+y,yc+x,WHITE);
+	putpixel(xc-y,yc+x,WHITE);
+	putpixel(xc+y,yc-x,WHITE);
+	putpixel(xc-y,yc-x,WHITE);
+	angle=angle+Tinc;
+	}
+}
+
+int main()
+{
+	int gd=DETECT,gm,a,b,c;
+
+initgraph(&gd,&gm," ");
+cout<<"enter coordinates"<<endl;
+	cout<<"X: ";
+	cin>>a;
+	cout<<"y: ";
+	cin>>b;
+	cout<<"enter radius ";
+	cin>>c;
+
+	circlepoly(a,b,c);
+	getch();
+	}
